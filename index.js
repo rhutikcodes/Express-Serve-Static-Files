dd10a5cf2e288c3765d80a437532286eb72b5e5c
Rhutik <rhutikgiradkar@gmail.com>
2021-12-20 17:35:22 +0530

    post route to upload files

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
 const ecstatic = require('ecstatic');
 
+app.use(fileUpload());
 
 app.use(ecstatic({
     root: `${__dirname}/public`,
     showdir: true,
 }));
+
+app.post('/upload', function (req, res) {
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    sampleFile = req.files.file;
+    uploadPath = __dirname + '/public/' + sampleFile.name;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+}) 
 app.listen(8080);
 console.log('Listening on port 8080');
 
